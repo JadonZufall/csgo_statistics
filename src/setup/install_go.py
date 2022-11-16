2a936781a278456c1ec300c5975dc6ba0fbfce05
Jadon <jadonzufall@gmail.com>
2022-11-16 00:07:02 -0500

    Added test case and modified setup file.

--- src/setup/install_go.py
+++ src/setup/install_go.py
@@ -1,6 +1,8 @@
 # Tools for insuring that golang is properly installed and or installing golang if not properly installed.
 import subprocess
 
+# TODO: Write functions for golang setup and installation.
+
 
 def ensure_go_install() -> bool:
     """ Ensures that golang is installed.
---
+++ src/tests/test_round_data.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+
+class TestRoundData(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_round_parsing(self) -> None:
+        # TODO: Write this test case.
+        pass
+
+    def test_empty_parsing(self) -> None:
+        # Test for certain variables being able to contain null / empty values
+        test_example = {}
